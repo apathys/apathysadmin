44a413bc027cdab936985b56fbf917f2e5116de5
apathys <568437570@qq.com>
2022-09-14 08:50:05 +0800

    初始版本EFData

---
+++ EFData/EFDataContext.cs
@@ -0,0 +1,21 @@
+﻿ 
+using Microsoft.EntityFrameworkCore;
+using XxsModel.SysBasic; 
+
+namespace EFData
+{
+    public class EFDataContext:DbContext
+    {
+        public EFDataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Sys_User> Sys_User { get; set; }
+        public DbSet<Sys_Role> Sys_Role { get; set; }
+        public DbSet<Sys_UserAndRole> Sys_UserAndRole { get; set; }
+        public DbSet<Sys_Menu> Sys_Menu { get; set; }
+        public DbSet<Sys_MenuAuthority> Sys_MenuAuthority { get; set; }
+        public DbSet<Sys_AuthorityRequest> Sys_AuthorityRequest { get; set; }
+        public DbSet<Sys_RoleAndAuthority> Sys_RoleAndAuthority { get; set; }
+    }
+}
